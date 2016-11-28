eceeafb3c5a48fad1e54df43022d3ed0235bb4b6
Yatao Li <yatao.li@live.com>
2016-11-29 01:59:08 +0800

    support ini configuration. add several config options.

--- DualCtrl32/DualCtrl32.cpp
+++ DualCtrl32/DualCtrl32.cpp
@@ -1,5 +1,8 @@
 #include "DualCtrl32.h"
 #include "UI.h"
+#include "..\inih\ini.h"
+#include "Shlwapi.h"
+#include "Strsafe.h"
 
 #if _DEBUG
 #define debug(x) do{printf(x);}while(false)
@@ -17,8 +20,28 @@ enum KeyboardEvent
     OTHER_KEY_DOWN,
     MOUSE_MOVE,
     MOUSE_BUTTON,
-    NONE, //Not interested
+    TIMEOUT,    //CTRL flush timer fired
+    NONE,       //Not interested, pass through
+    INTERCEPT,  //Unknown, intercept
 };
+
+#define DC32_CONFIG_FILENAME     "\\DualCtrl32.ini"
+#define DC32_CTRLFLUSH_TIMEOUT   (10086)
+#define DC32_PATHMAX             (1024)
+
+//============ Configuration ============
+
+#define CONFIG_CTRLFLUSH_TIMEOUT "TimerTrigger"
+#define CONFIG_CTRLFLUSH_MOUSE   "MouseTrigger"
+#define CONFIG_CTRLFLUSH_KEY     "KeyTrigger"
+#define CONFIG_ALTDRAG_FIX       "AltDragFix"
+
+UINT          g_ctrlflush_timeout = 0;     // 0: Disable; >0: force flush CTRL-DOWN event after timeout
+BOOL          g_ctrlflush_mouse   = TRUE;  // FALSE: do not flush CTRL-DOWN event on mouse movement
+BOOL          g_ctrlflush_key     = TRUE;  // FALSE: do not flush CTRL-DOWN event on key strokes
+BOOL          g_altdrag_fix       = TRUE;  // FALSE: Disable AltDrag fix.
+
+//============ State registers ============
 
 BOOL          g_ctrl_down         = FALSE; // TRUE if CTRL is DOWN
 BOOL          g_ctrl_flushed      = FALSE; // TRUE if CTRL-DOWN is flushed, and we should send CTRL-UP when the key is released
@@ -26,12 +49,83 @@ BOOL          g_ctrl_thru         = FALSE; // TRUE if we intentionally bypass ou
 BOOL          g_alt_down          = FALSE; // TRUE if ALT is DOWN
 BOOL          g_altdrag_activated = FALSE; // TRUE if we infer the activation of AltDrag (currently not working)
 BOOL          g_altdrag_ctrlseq   = FALSE; // TRUE if we detect AltDrag behavior sequence that needs a fix
+BOOL          g_ignore_timer      = FALSE; // TRUE if timer should be ignored (CTRL-UP received before timer fires)
+
+VOID             LoadConfig();
+BOOL             ReadBool(LPCSTR);
+INT              ReadConfigEntry(LPVOID, LPCSTR, LPCSTR, LPCSTR);
+VOID CALLBACK    CtrlTimeout(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime);
+KeyboardEvent    GetEvent(int nCode, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK DualCtrl32(int nCode, WPARAM wParam, LPARAM lParam);
+
+VOID LoadConfig()
+{
+    debug("Loading config\n");
+    HMODULE hModule = GetModuleHandle(NULL);
+    if (hModule == NULL)
+    {
+        debug("hModule\n");
+        return;
+    }
+
+    CHAR path[DC32_PATHMAX];
+    if (DC32_PATHMAX == GetModuleFileNameA(hModule, path, DC32_PATHMAX))
+    {
+        debug("wPath\n");
+        return;
+    }
+    PathRemoveFileSpecA(path);
+    if (S_OK != StringCbCatA(path, DC32_PATHMAX, DC32_CONFIG_FILENAME))
+    {
+        debug("StringCbCat\n");
+        return;
+    }
+
+    debug("Config path = "); debug(path); debug("\n");
+    if (ini_parse(path, ReadConfigEntry, NULL))
+    {
+        debug("ini_parse error\n");
+    }
+}
+
+#define MATCH(x) strcmp(x, key) == 0
+
+BOOL ReadBool(LPCSTR key)
+{
+    return MATCH("TRUE") || MATCH("True") || MATCH("true") ||
+           MATCH("Enabled") || MATCH("ENABLED");
+}
+
+INT ReadConfigEntry(LPVOID o, LPCSTR section, LPCSTR key, LPCSTR value)
+{
+    UNREFERENCED_PARAMETER(o);
+    UNREFERENCED_PARAMETER(section);
+
+    debug("Load config entry: "); debug(key); debug("="); debug(value); debug("\n");
+
+    if (MATCH(CONFIG_CTRLFLUSH_KEY)) { g_ctrlflush_key = ReadBool(value); }
+    if (MATCH(CONFIG_CTRLFLUSH_MOUSE)) { g_ctrlflush_mouse = ReadBool(value); }
+    if (MATCH(CONFIG_CTRLFLUSH_TIMEOUT)) { g_ctrlflush_timeout = atoi(value); }
+    if (MATCH(CONFIG_ALTDRAG_FIX)) { g_altdrag_fix = ReadBool(value); }
+
+    return 1;
+}
+
+VOID CALLBACK CtrlTimeout(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
+{
+    debug("Timer fired\n");
+    KillTimer(hwnd, idEvent);
+    if (!g_ignore_timer)
+    {
+        DualCtrl32(DC32_CTRLFLUSH_TIMEOUT, NULL, NULL);
+    }
+}
 
 KeyboardEvent GetEvent(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
     if (nCode == HC_ACTION)
     {
+        PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
         switch (wParam)
         {
         case WM_KEYDOWN:
@@ -40,7 +134,7 @@ KeyboardEvent GetEvent(int nCode, WPARAM wParam, LPARAM lParam)
             {
             case VK_LCONTROL: return KeyboardEvent::CTRL_DOWN;
             case VK_LMENU: return KeyboardEvent::ALT_DOWN;
-            default: return KeyboardEvent::OTHER_KEY_DOWN;
+            default: if (g_ctrlflush_key) return KeyboardEvent::OTHER_KEY_DOWN;
             }
         case WM_KEYUP:
         case WM_SYSKEYUP:
@@ -48,21 +142,26 @@ KeyboardEvent GetEvent(int nCode, WPARAM wParam, LPARAM lParam)
             {
             case VK_LCONTROL: return KeyboardEvent::CTRL_UP;
             case VK_LMENU: return KeyboardEvent::ALT_UP;
-            default: return KeyboardEvent::OTHER_KEY_UP;
+            default: if (g_ctrlflush_key) return KeyboardEvent::OTHER_KEY_UP;
             }
         case WM_MOUSEMOVE:
-            return KeyboardEvent::MOUSE_MOVE;
+            if (g_ctrlflush_mouse) return KeyboardEvent::MOUSE_MOVE;
         case WM_LBUTTONDOWN:
         case WM_LBUTTONUP:
         case WM_MOUSEWHEEL:
         case WM_MOUSEHWHEEL:
         case WM_RBUTTONDOWN:
         case WM_RBUTTONUP:
-            return KeyboardEvent::MOUSE_BUTTON;
+            if (g_ctrlflush_mouse) return KeyboardEvent::MOUSE_BUTTON;
         default:
             return KeyboardEvent::NONE;
         }
     }
+    else if (nCode == DC32_CTRLFLUSH_TIMEOUT)
+    {
+        return KeyboardEvent::TIMEOUT;
+    }
+    return KeyboardEvent::INTERCEPT;
 }
 
 LRESULT CALLBACK DualCtrl32(int nCode, WPARAM wParam, LPARAM lParam)
@@ -93,6 +192,11 @@ LRESULT CALLBACK DualCtrl32(int nCode, WPARAM wParam, LPARAM lParam)
         {
             debug("Cache ctrl-down event.\n");
             g_ctrl_down = TRUE;
+            if (g_ctrlflush_timeout > 0)
+            {
+                g_ignore_timer = FALSE;
+                SetTimer(NULL, DC32_CTRLFLUSH_TIMEOUT, g_ctrlflush_timeout, CtrlTimeout);
+            }
         }
         intercept = TRUE;
         break;
@@ -101,12 +205,13 @@ LRESULT CALLBACK DualCtrl32(int nCode, WPARAM wParam, LPARAM lParam)
         {
             g_ctrl_down = FALSE;
             intercept   = TRUE;
-            if (!g_alt_down && !g_altdrag_ctrlseq)
+            if (!g_altdrag_fix || (!g_alt_down && !g_altdrag_ctrlseq))
             {
                 //  Translate to ESC
                 debug("Translate to ESC.\n");
                 keybd_event(VK_ESCAPE, 0x81, 0, 0);
                 keybd_event(VK_ESCAPE, 0x81, KEYEVENTF_KEYUP, 0);
+                g_ignore_timer = TRUE;
             }
             else if (g_alt_down)
             {
@@ -126,7 +231,7 @@ LRESULT CALLBACK DualCtrl32(int nCode, WPARAM wParam, LPARAM lParam)
         {
             debug("Flush complete, CTRL released.\n");
             g_ctrl_flushed = FALSE;
-            g_ctrl_thru    = TRUE;
+            g_ctrl_thru = TRUE;
             keybd_event(VK_LCONTROL, 0x9D, KEYEVENTF_KEYUP, 0);
             intercept = TRUE;
         }
@@ -147,17 +252,18 @@ LRESULT CALLBACK DualCtrl32(int nCode, WPARAM wParam, LPARAM lParam)
         /* FALLTHROUGH */
     case KeyboardEvent::MOUSE_MOVE:
     case KeyboardEvent::OTHER_KEY_DOWN:
+    case KeyboardEvent::TIMEOUT:
         if (g_ctrl_down)
         {
             //  Flush the cached ctrl-down event
             debug("Flush ctrl-down.\n");
             g_ctrl_down    = FALSE;
-            g_ctrl_thru    = TRUE;
             g_ctrl_flushed = TRUE;
+            g_ctrl_thru    = TRUE;
             keybd_event(VK_LCONTROL, 0x9D, 0, 0);
-            // keybd_event('B', 0, KEYEVENTF_KEYUP, 0);
         }
-        intercept = FALSE;
+        //  Intercept TIMEOUT event as it is not a real hook message
+        intercept = (event == KeyboardEvent::TIMEOUT);
         break;
     case KeyboardEvent::OTHER_KEY_UP:
     case KeyboardEvent::NONE:
@@ -177,14 +283,17 @@ LRESULT CALLBACK DualCtrl32(int nCode, WPARAM wParam, LPARAM lParam)
         break;
     case KeyboardEvent::ALT_UP:
         debug("Alt up\n");
-        intercept           = FALSE;
-        g_alt_down          = FALSE;
+        intercept  = FALSE;
+        g_alt_down = FALSE;
         if (g_altdrag_activated)
         {
             //  Expect AltDrag sequence II
             g_altdrag_ctrlseq = TRUE;
         }
         g_altdrag_activated = FALSE;
+        break;
+    case KeyboardEvent::INTERCEPT:
+        intercept = TRUE;
         break;
     }
 
@@ -199,19 +308,21 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // Install the low-level keyboard & mouse hooks
-    HHOOK hhkLLKeyboard = SetWindowsHookEx(WH_KEYBOARD_LL, DualCtrl32, 0, 0);
-    HHOOK hhkLLMouse    = SetWindowsHookEx(WH_MOUSE_LL, DualCtrl32, 0, 0);
-
-    SetupUI(hInstance);
-
-    MSG msg;
-
 #if _DEBUG
     AllocConsole();
     FILE* fp;
     freopen_s(&fp, "CONOUT$", "w", stdout);
 #endif
+
+
+    // Install the low-level keyboard & mouse hooks
+    HHOOK hhkLLKeyboard = SetWindowsHookEx(WH_KEYBOARD_LL, DualCtrl32, 0, 0);
+    HHOOK hhkLLMouse    = SetWindowsHookEx(WH_MOUSE_LL, DualCtrl32, 0, 0);
+
+    LoadConfig();
+    SetupUI(hInstance);
+
+    MSG msg;
 
     // Main message loop:
     while (GetMessage(&msg, NULL, 0, 0))
--- README.md
+++ README.md
@@ -7,3 +7,10 @@ After starting the program, LCONTROL becomes a dual-role key.
 - Press it down, and then release it without doing anything else on the keyboard/with the mouse, will translate LCOLTROL into ESC.
 - Press it down, and perform other keyboard/mouse actions (like move mouse, press other keys), and the cached LCTRL-DOWN event will be flushed.
   - This is different from most other dual-role key programs in that DualCtrl32 is aware of mouse events (and also alt-drag events)
+
+## Configuration
+Place DualCtrl32.ini beside the executable.
+- `KeyTrigger=FALSE` disables keyboard event flush trigger.
+- `MouseTrigger=FALSE` disables mouse event flush trigger.
+- `TimerTrigger=100` sets a timeout flush trigger after LCTRL-DOWN, recommend use with disabled key/mouse trigger.
+- `AltDragFix=FALSE` disables AltDrag fix.
